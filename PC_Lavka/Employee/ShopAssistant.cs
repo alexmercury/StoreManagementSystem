2922031399407c29c4f061a73301ee7427247ce4
Alex <alexsummers@yandex.ua>
2013-11-26 23:53:47 +0200

    Add Employee classes

---
+++ PC_Lavka/Employee/ShopAssistant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Employee
+{
+  public class ShopAssistant : User
+  {
+
+    public ShopAssistant()
+    {
+      this.ID = 0;
+      this.UID = 0;
+    }
+
+  }
+
+}
