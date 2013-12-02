2b8607243d724f7245cbf9c8c0ae45af47110e9a
Alex <alexsummers@yandex.ua>
2013-12-02 04:09:16 +0200

    Fix SQL scripts

--- PC_Lavka/data/install/scripts/products.sql
+++ PC_Lavka/data/install/scripts/products.sql
@@ -3,7 +3,7 @@
 	[id] INT IDENTITY(1,1) PRIMARY KEY,
 	[name] VARCHAR(50) NOT NULL,
 	[specification] VARCHAR(MAX) NOT NULL,
-	[image_url] VARCHAR(100) NOT NULL,
+	[photo] IMAGE,
 	[price] INT NOT NULL,
 	[vendor_id] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[vendors]([id]),
 	[category_id] INT NOT NULL FOREIGN KEY REFERENCES [dbo].[categories]([id])
--- PC_Lavka/data/install/scripts/users.sql
+++ PC_Lavka/data/install/scripts/users.sql
@@ -2,10 +2,10 @@
 (
 	[id] INT IDENTITY(1,1) PRIMARY KEY,
 	[uid] INT NOT NULL UNIQUE,
-	[password] VARCHAR(255) NOT NULL, 
+	[password] VARCHAR(20) NOT NULL, 
 	[surname] VARCHAR(50) NOT NULL,
 	[name] VARCHAR(50) NOT NULL ,
 	[patronymic] VARCHAR(50) NOT NULL,
-	[image_url] VARCHAR(100) NOT NULL,
+	[photo] IMAGE,
 	[is_admin] BIT DEFAULT 'FALSE' 
 )
